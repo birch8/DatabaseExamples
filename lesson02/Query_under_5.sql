40d5b583d24bd22df11e97aaa2707e9657abf75c
birch8 <210781416+birch8@users.noreply.github.com>
2025-11-26 23:54:18 +0000

    Fix data types so queries can run, repopulate sweets.db

--- lesson02/Query_under_5.sql
+++ lesson02/Query_under_5.sql
@@ -1,3 +1,2 @@
-SELECT category_tbl.category_name, sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id
-FROM category_tbl INNER JOIN sweet_tbl ON category_tbl.category_id = Sweet_tbl.category_id 
-WHERE sweet_tbl.price <= '$5.00';
+SELECT category_tbl.category_name, sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id 
+FROM category_tbl INNER JOIN sweet_tbl ON category_tbl.category_id = sweet_tbl.category_id WHERE sweet_tbl.price <= 5.00;
