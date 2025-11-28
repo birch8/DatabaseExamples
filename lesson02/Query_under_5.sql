14a25f06d1e09aa0dd36f195d3b59c47f0b7bcdf
birch8 <210781416+birch8@users.noreply.github.com>
2025-11-28 12:02:44 +1100

    Update Query_under_5.sql

--- lesson02/Query_under_5.sql
+++ lesson02/Query_under_5.sql
@@ -1,4 +1,4 @@
--- Find sweets in any category that are under $5.00.
+-- Find sweets in any category that are under $5.00. (Task 1)
 .header ON
 .mode column
 SELECT category_tbl.category_name, sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id 
