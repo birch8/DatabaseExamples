bbada8f42a11e6373433d5c6bf16cfdf39d01323
birch8 <210781416+birch8@users.noreply.github.com>
2025-11-28 11:57:54 +1100

    Add comment and fix price query

--- lesson02/Query_under_5_and.sql
+++ lesson02/Query_under_5_and.sql
@@ -1,4 +1,5 @@
+-- Find sweets that are either Chocolate or Toffee that are under $5.00 (task 2)
 SELECT category_tbl.category_name, sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id
 FROM category_tbl INNER JOIN sweet_tbl ON category_tbl.category_id = Sweet_tbl.category_id 
-WHERE sweet_tbl.price <= '$5.00' and category_tbl.category_name = 'Chocolate' or category_name = 'Toffee'
+WHERE sweet_tbl.price <= 5.00 and category_tbl.category_name = 'Chocolate' or category_name = 'Toffee'
 ORDER BY sweet_tbl.sweet_name DESC;
