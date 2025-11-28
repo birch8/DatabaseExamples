beafbab3f782d230386137661724724d8e30a98c
birch8 <210781416+birch8@users.noreply.github.com>
2025-11-28 12:22:44 +1100

    Update Query_under_5_sum.sql
    Get rid of sum because it was confusing and now it shows all sweets under 5 dollars

--- lesson02/Query_under_5_sum.sql
+++ lesson02/Query_under_5_sum.sql
@@ -1,2 +1,5 @@
-SELECT sum(sweet_tbl.price), sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id from sweet_tbl
-WHERE sweet_tbl.price <= '$5.00' 
+-- sweeets under 5 dollars
+.headers ON
+.mode column
+SELECT sweet_tbl.sweet_ID, sweet_tbl.sweet_name, sweet_tbl.bag_weight, sweet_tbl.price, sweet_tbl.image, sweet_tbl.category_id from sweet_tbl
+WHERE sweet_tbl.price <= 5.00 
